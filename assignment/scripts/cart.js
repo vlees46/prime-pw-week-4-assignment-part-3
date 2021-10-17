14fef36f7a76b65f11521559f5984a78f63af6d1
Victor Lees <vlees14.pa@gmail.com>
2021-10-17 13:41:07 -0400

    Initial commit adding last function to empty array

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,32 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+
+let basket = [];
+
+function addItem(item){
+
+if (basket.push(item)) {
+
+    return true
+}
+else {
+    return false;
+}
+}
+console.log(`Basket is ${basket}`);
+console.log('Adding pizza', addItem('pizza'));
+console.log('Adding pizza', addItem('nachos'));
+console.log('Adding pizza', addItem('frys'));
+console.log(`Basket is now ${basket}`);
+
+
+function listItems (){
+
+    for (i=0; i < basket.length; i++){
+
+     console.log('in the basket is', basket[i]);
+
+    }
+}
+listItems();
